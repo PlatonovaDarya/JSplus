76bc32783c9ea0d444014b44389ae6d67c1a418a
PlatonovaDarya <platonova_d@icloud.com>
2024-10-02 21:53:24 +0300

    Все функции работают

--- scripts/scriptplus.js
+++ scripts/scriptplus.js
@@ -1,19 +1,129 @@
-
-document.addEventListener('DOMContentLoaded', () => {
+const totalBooks = document.getElementById("total");
+document.addEventListener("DOMContentLoaded", () => {
     loadBooks();
 });
+
+function addBook() {
+    const input1 = document.getElementById("titleBook");
+    const input2 = document.getElementById("author");
+    const input3 = document.getElementById("year");
+    const bookList = document.getElementById("bookList");
+    const li = document.createElement("li");
+    li.insertAdjacentHTML(
+        "beforeend",
+        `
+            <span class="btn">${input1.value} ${input2.value} ${input3.value}</span>
+                <button onclick="editBook(this)" class="btnGrey" id="through" data-type="readIt">&#128270;</button>
+                <button onclick="deleteBook(this)" class="btnRed" data-type="delete">&#10060;</button>
+                <button onclick="doneBook(this)" class="btnGreen" data-type="done">&#10004;</button>
+            `
+    );
+    bookList.appendChild(li);
+    (input1.value = ""), (input2.value = ""), (input3.value = "");
+    saveBooks();
+    filterBooks();
+}
+
+function deleteBook(button) {
+    const li = button.parentElement;
+    li.remove();
+    saveBooks();
+    filterBooks();
+}
+
+function editBook(button) {
+    const li = button.parentElement;
+    const bookText = li.querySelector("span").textContent;
+    const newBookText = prompt("Редактировать", bookText);
+
+    if (newBookText !== null && newBookText.trim() !== "") {
+        li.querySelector("span").textContent = newBookText.trim();
+        saveBooks();
+        filterBooks();
+    }
+}
+
+function saveBooks() {
+    const bookList = document.getElementById("bookList");
+    const books = Array.from(bookList.children).map(
+        (li) => li.querySelector("span").textContent
+    );
+    totalBooks.innerHTML = books.length;
+
+    localStorage.setItem("books", JSON.stringify(books));
+}
+
+function loadBooks() {
+    const books = JSON.parse(localStorage.getItem("books")) || [];
+    const bookList = document.getElementById("bookList");
+
+    totalBooks.innerHTML = books.length;
+
+    books.forEach((book) => {
+        const li = document.createElement("li");
+        li.insertAdjacentHTML(
+            "beforeend",
+            `
+                <span class="btn">${book}</span>
+                    <button onclick="editBook(this)" class="btnGrey" data-type="readIt">&#128270;</button>
+                    <button onclick="deleteBook(this)" class="btnRed" data-type="delete">&#10060;</button>
+                    <button onclick="doneBook(this)" class="btnGreen" data-type="done">&#10004;</button>
+            `
+        );
+        bookList.appendChild(li);
+    });
+    filterBooks();
+}
+
+function doneBook(button) {
+    const li = button.parentElement;
+    li.style.setProperty("text-decoration", "line-through");
+    saveBooks();
+}
+
+function delAllBooks(button) {
+    const deleteElement = document.querySelector("#bookList");
+    totalBooks.innerHTML = 0;
+
+    deleteElement.innerHTML = "";
+}
+
+function filterBooks() {
+    const filterBook = document.getElementById("filterBook");
+    const filterText = filterBook.value.toLowerCase();
+    const bookList = document.getElementById("bookList");
+
+    Array.from(bookList.children).forEach((li) => {
+        const bookText = li.querySelector("span").textContent.toLowerCase();
+        if (bookText.includes(filterText)) {
+            li.style.display = "";
+        } else {
+            li.style.display = "none";
+        }
+    });
+}
+
+function reloadPage() {
+    location.reload();
+}
+
+
+
+
+
+
+
+
+/*document.addEventListener('DOMContentLoaded', () => {
+    loadBooks();
+});
+
 
 function addBook() {
     const input1 = document.getElementById('titleBook');
     const input2 = document.getElementById('author');
     const input3 = document.getElementById('year');
-
-    if (input1.value.length === '', input2.value.length === '', input3.value.length === '') {
-        alert("Вы ничего не ввели!");
-    }
-
     const bookList = document.getElementById('bookList');
-    
     const li = document.createElement('li');
     li.insertAdjacentHTML('beforeend',
         `
@@ -23,16 +133,17 @@ function addBook() {
                 <button onclick="doneBook(this)" class="btnGreen" data-type="done">&#10004;</button>
             `
     );
-
     bookList.appendChild(li);
-    saveBooks();
     input1.value = '', input2.value = '', input3.value = '';
+    saveBooks();
+    filterBooks();
 }
 
 function deleteBook(button) {
     const li = button.parentElement;
     li.remove();
     saveBooks();
+    filterBooks();
 }
 
 function editBook(button) {
@@ -43,6 +154,7 @@ function editBook(button) {
     if (newBookText !== null && newBookText.trim() !== '') {
         li.querySelector('span').textContent = newBookText.trim();
         saveBooks();
+        filterBooks();
     }
 }
 
@@ -67,6 +179,7 @@ function loadBooks() {
             `);
         bookList.appendChild(li);
     });
+    filterBooks();
 }
 
 function doneBook(button) {
@@ -79,3 +192,19 @@ function delAllBooks(button) {
     const deleteElement = document.querySelector("#bookList");
     deleteElement.innerHTML = '';
 }
+
+function filterBooks() {
+    const filterBook = document.getElementById('filterBook');
+    const filterText = filterBook.value.toLowerCase();
+    const bookList = document.getElementById('bookList');
+
+    Array.from(bookList.children).forEach(li => {
+        const bookText = li.querySelector('span').textContent.toLowerCase();
+        if (bookText.includes(filterText)) {
+            li.style.display = '';
+        } else {
+            li.style.display = 'none';
+        }
+    });
+}*/
+
